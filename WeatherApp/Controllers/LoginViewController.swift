e64113b9d98b7de92af6f631b4ec9b026d4dc913
Dara adekore <adekore8@gmail.com>
2023-02-22 20:05:41 -0800

    files

--- WeatherApp/Controllers/LoginViewController.swift
+++ WeatherApp/Controllers/LoginViewController.swift
@@ -89,7 +89,7 @@ class LoginViewController: UIViewController {
                     goToMainAppScreen()
                 }
                 else {
-                    let ac = UIAlertController(title: "Wrong passgiword", message: "", preferredStyle: .alert)
+                    let ac = UIAlertController(title: "Wrong passgigitword", message: "", preferredStyle: .alert)
                     ac.addAction(UIAlertAction(title: "Ok", style: .cancel))
                     present(ac,animated: true)
                 }
